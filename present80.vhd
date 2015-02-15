bcb6fe6869538f3163c4db28084982deaf3d6a9e
arquimago <thales@alvesnet.net>
2015-02-15 15:55:48 -0300

    Ajustei a entrada que é 64 bits e não 8

--- present80.vhd
+++ present80.vhd
@@ -4,9 +4,9 @@ use IEEE.std_logic_1164.all;
 entity present80 is
  port (
  key: in STD_LOGIC_VECTOR (79 downto 0);
- bloco: in STD_LOGIC_VECTOR (7 downto 0);
+ bloco: in STD_LOGIC_VECTOR (63 downto 0);
  rst,clk: in STD_LOGIC;
- saida: out STD_LOGIC_VECTOR (7 downto 0)
+ saida: out STD_LOGIC_VECTOR (63 downto 0)
  );
 end present80;
 
--- sBoxLayer.vhd
+++ sBoxLayer.vhd
@@ -3,8 +3,8 @@ use IEEE.std_logic_1164.all;
 
 entity sBoxLayer is
  port (
- bloco: in STD_LOGIC_VECTOR (7 downto 0);
+ bloco: in STD_LOGIC_VECTOR (63 downto 0);
  rst,clk: in STD_LOGIC;
- saida: out STD_LOGIC_VECTOR (7 downto 0)
+ saida: out STD_LOGIC_VECTOR (63 downto 0)
  );
 end sBoxLayer;

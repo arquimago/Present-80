e8eb027dcf51b715e5979e64e7ada703aacecb9b
arquimago <thales@alvesnet.net>
2015-02-24 14:01:40 -0300

    revertendo
    ...

--- present80.vhd
+++ present80.vhd
@@ -35,26 +35,26 @@ end component;
 component genRoundKey
 port (
 	key: in STD_LOGIC_VECTOR (79 downto 0);
-	bloco: in STD_LOGIC_VECTOR (63 downto 0);
 	round_counter: in STD_LOGIC_VECTOR(4 downto 0);	
 	saida: out STD_LOGIC_VECTOR (79 downto 0)
 );
 end component;
 
-<<<<<<< HEAD
 
-variable chave_aux: STD_LOGIC_VECTOR(79 downto 0);
+signal chave_aux: STD_LOGIC_VECTOR(79 downto 0);
 variable entrada_aux1,entrada_aux2: STD_LOGIC_VECTOR(63 downto 0);
 variable chave: STD_LOGIC_VECTOR(79 downto 0);
 variable contador: STD_LOGIC_VECTOR(4 downto 0);
 
 begin
-entrada_aux1<=bloco;
+entrada_aux1<= bloco;
 chave<=key;
 contador<="00000";
 contador<=contador+1;
 chave_aux<=key;
 
+process(key)
+begin
 	for i in 1 to 31 loop
 		ark: addRoundKey port map (chave_aux,entrada_aux1,entrada_aux2);
 		sbl: sBoxLayer port map (entrada_aux2,entrada_aux1);
@@ -65,13 +65,5 @@ chave_aux<=key;
 		chave_aux<=chave;		
 	end loop;
 	ark2: addRoundKey port map (chave,entrada_aux1,saida);
-
-=======
-begin
-	begin
-	for i in 0 to 30 loop
-		--aqui chamamos as coisas :P
-	end loop;
-	-- aqui a gente chama o addroundKey pela ultima vez
->>>>>>> parent of 796cd56... "main" quase pronto
+	end process;
 end present80_arch;
